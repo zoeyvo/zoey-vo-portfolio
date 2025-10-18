f43e9390e507934bf732ab475beae930e70fab82
zoeyvo <zoeyvo256@gmail.com>
2025-10-18 00:12:34 -0700

    add picture

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,6 +1,7 @@
 const Bio = () => (
   <div className="grid-cell">
     <div className="content">
+      <img className="bio-picture" src="../assets/images/zoey.jpg"></img>
       <h1 className="title">
         <span className="bio-title-desktop">zoey vo · 🏳️‍⚧️ · she/her</span>
         <div className="bio-title-mobile">
@@ -9,6 +10,7 @@ const Bio = () => (
           <span className="bio-mobile-item">she/her</span>
         </div>
       </h1>
+
       <span className="bio-desc">
         Developer committed to building technology that serves people. Focused
         on systems and applications that meet real human needs, with a passion
